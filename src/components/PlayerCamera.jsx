b12c470ba195b4cf5da19f8ecf5a60339dfa0c59
Jeremy Tsai <jtsai@purestorage.com>
2021-12-02 15:51:10 -0800

    Add start/pause button functionality

--- src/components/PlayerCamera.jsx
+++ src/components/PlayerCamera.jsx
@@ -1,47 +1,38 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import ml5 from 'ml5';
-// import useInterval from '@use-it/interval';
 import Sketch from 'react-p5';
-/* eslint-disable */
 
 const PlayerCamera = function PlayerCamera() {
-  let video;
-  let poseNet;
-  let pose;
-  let skeleton;
+  const video = useRef();
+  const poseNet = useRef();
+  const pose = useRef();
+  const skeleton = useRef();
 
-  let brain;
-  let poseLabel = "Y";
+  const brain = useRef();
+  const [poseLabel, setPoseLabel] = useState('N');
 
-  const setup = (p5, canvasParentRef) => {
-    p5.createCanvas(640, 480).parent(canvasParentRef);
-    video = p5.createCapture(p5.VIDEO);
-    video.hide();
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
+  const classifyPose = (p5) => {
+    if (pose.current) {
+      const gotResult = (error, results) => {
+        if (results[0].confidence > 0.75) {
+          setPoseLabel(results[0].label.toUpperCase());
+        }
+        // console.log(results[0].confidence);
+        classifyPose();
+      };
 
-    let options = {
-      inputs: 34,
-      outputs: 4,
-      task: 'classification',
-      debug: true
+      const inputs = [];
+      for (let i = 0; i < pose.current.keypoints.length; i += 1) {
+        const { x } = pose.current.keypoints[i].position;
+        const { y } = pose.current.keypoints[i].position;
+        inputs.push(x);
+        inputs.push(y);
+      }
+      brain.current.classify(inputs, gotResult);
+    } else {
+      p5.setTimeout(classifyPose(), 100);
     }
-
-    brain = ml5.neuralNetwork(options);
-    const modelInfo = {
-      model: './model/model.json',
-      metadata: './model/model_metadata.json',
-      weights: './model/model.weights.bin',
-    };
-    brain.load(modelInfo, brainLoaded);
   };
-
-  function gotPoses(poses) {
-    if (poses.length > 0) {
-      pose = poses[0].pose;
-      skeleton = poses[0].skeleton;
-    }
-  }
 
   function modelLoaded() {
     console.log('poseNet ready');
@@ -52,47 +43,65 @@ const PlayerCamera = function PlayerCamera() {
     classifyPose(p5);
   };
 
-  const classifyPose = (p5) => {
-    if (pose) {
-      let inputs = [];
-      for (let i = 0; i < pose.keypoints.length; i++) {
-        let x = pose.keypoints[i].position.x;
-        let y = pose.keypoints[i].position.y;
-        inputs.push(x);
-        inputs.push(y);
-      }
-      brain.classify(inputs, gotResult);
-    } else {
-      p5.setTimeout(classifyPose(), 100);
+  function gotPoses(poses) {
+    if (poses.length > 0) {
+      pose.current = poses[0].pose;
+      skeleton.current = poses[0].skeleton;
     }
-  };
-
-  function gotResult(error, results) {
-    if (results[0].confidence > 0.75) {
-      poseLabel = results[0].label.toUpperCase();
-    }
-    //console.log(results[0].confidence);
-    classifyPose();
   }
+
+  const setup = (p5, canvasParentRef) => {
+    p5.createCanvas(640, 480).parent(canvasParentRef);
+    video.current = p5.createCapture(p5.VIDEO);
+    video.current.hide();
+    poseNet.current = ml5.poseNet(video, modelLoaded);
+    poseNet.current.on('pose', gotPoses);
+
+    const options = {
+      inputs: 34,
+      outputs: 4,
+      task: 'classification',
+      debug: true,
+    };
+
+    brain.current = ml5.neuralNetwork(options);
+    const modelInfo = {
+      model: './model/model.json',
+      metadata: './model/model_metadata.json',
+      weights: './model/model.weights.bin',
+    };
+    brain.current.load(modelInfo, brainLoaded);
+  };
 
   const draw = (p5) => {
     p5.push();
-    p5.translate(video.width, 0);
+    p5.translate(video.current.width, 0);
     p5.scale(-1, 1);
-    p5.image(video, 0, 0, video.width, video.height);
+    p5.image(
+      video.current,
+      0,
+      0,
+      video.current.width,
+      video.current.height,
+    );
 
-    if (pose) {
-      for (let i = 0; i < skeleton.length; i++) {
-        let a = skeleton[i][0];
-        let b = skeleton[i][1];
+    if (pose.current) {
+      for (let i = 0; i < skeleton.current.length; i += 1) {
+        const a = skeleton.current[i][0];
+        const b = skeleton.current[i][1];
         p5.strokeWeight(2);
         p5.stroke(0);
 
-        p5.line(a.position.x, a.position.y, b.position.x, b.position.y);
+        p5.line(
+          a.position.x,
+          a.position.y,
+          b.position.x,
+          b.position.y,
+        );
       }
-      for (let i = 0; i < pose.keypoints.length; i++) {
-        let x = pose.keypoints[i].position.x;
-        let y = pose.keypoints[i].position.y;
+      for (let i = 0; i < pose.current.keypoints.length; i += 1) {
+        const { x } = pose.current.keypoints[i].position;
+        const { y } = pose.current.keypoints[i].position;
         p5.fill(0);
         p5.stroke(255);
         p5.ellipse(x, y, 16, 16);
@@ -107,9 +116,7 @@ const PlayerCamera = function PlayerCamera() {
     p5.text(poseLabel, p5.width / 2, p5.height / 2);
   };
 
-  return (
-    <Sketch setup={setup} draw={draw}></Sketch>
-  )
+  return <Sketch setup={setup} draw={draw} />;
 };
 
 export default PlayerCamera;
--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
 import { makeStyles } from '@material-ui/core/styles';
+import { GameContext } from '../context/GameContext';
 
 const useStyles = makeStyles((theme) => ({
   startButton: {
@@ -11,17 +13,35 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const HelpButton = function HelpButton() {
+  const gameContext = useContext(GameContext);
   const classes = useStyles();
+  const [isPlaying, setIsPlaying] = useState(
+    gameContext.gameStats.isPlaying,
+  );
+
+  const handleStart = () => {
+    const newGameStats = { ...gameContext.gameStats };
+    newGameStats.isPlaying = !newGameStats.isPlaying;
+
+    gameContext.setGameStats(newGameStats);
+    setIsPlaying(newGameStats.isPlaying);
+  };
 
   return (
     <Button
       variant="outlined"
       color="primary"
       className={classes.startButton}
-      startIcon={<PlayCircleOutlineIcon />}
-      onClick={() => {}}
+      startIcon={
+        isPlaying ? (
+          <PauseCircleOutlineIcon />
+        ) : (
+          <PlayCircleOutlineIcon />
+        )
+      }
+      onClick={handleStart}
     >
-      Start
+      {isPlaying ? 'Pause' : 'Start'}
     </Button>
   );
 };
